6eb097b8df4a7420720a463f939ae97dceacba7a
Longs Pemun Gotar <developerlongs@gmail.com>
2022-10-28 16:59:13 +0000

    implemented change pin functionality

--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 
 func main(){
 	var pin int
+	var changedPin int
+	var userOption int
+
 	fmt.Println("Hello dear user, Welcome to the ATM machine")
 	
+	// ask user for a numeric pin
 	fmt.Println("Please, kindly type your pin")
 	fmt.Scanln(&pin)
 
-	fmt.Println(pin)
+	fmt.Println("Press 1 to change pin")
+	fmt.Println("Press 2 to check account balance")
+	fmt.Println("Press 3 to withdraw funds")
+	fmt.Println("Press 4 to deposit funds")
+
+	fmt.Scanln(&userOption)
+
+	if userOption == 1 {
+		fmt.Println("Type a four digit number as your new pin")
+		fmt.Scanln(&changedPin)
+		fmt.Printf("Your old pin %v is successfully changed to your new pin %v\n", pin, changedPin)
+		pin = changedPin
+	}
 
 }
 
